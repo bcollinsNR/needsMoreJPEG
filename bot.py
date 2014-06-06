19a1bda0c82fc3d8911123b79cea49aac2094de3
Brian Collins <bcollins@newrelic.com>
2014-06-05 20:47:41 -0700

    parsing comments

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
+import time
 import praw
 from PIL import Image
 import os
+
+#picture compression stuff works so working on the reddit interaction now
 
 #picture = Image.open("test.jpg")
 #newPic = picture.save("/Users/bcollins/Desktop/test_compressed.jpg", quality=1)
@@ -12,34 +15,23 @@ import os
 #url = open('url.txt', 'r').readline()
 #print url
 
-# todo: reddit API bot functionality
-
-#  get the image file
-#  compress to 1 quality and save copy locally
-#  upload to imgur, save url 
-#  craft response 
-#    get some template from somewhere 
-#  post to reddit
-
-
 r = praw.Reddit('needsMoreJPEGBot/0.1 by bcollinsNR'
                 'Url: https://github.com/bcollinsNR/needsMoreJPEG')
 r.login('needMoarJPEG','0111000001100001011100110111001101110111011011110111001001100100')
-#already_done = []
-
-print(dir(r.get_redditor("needMoarJPEG")))
+already_done = []
 
 
-
-#prawWords = ['needs', 'more', 'jpeg']
-#while True:
-#    subreddit = r.get_subreddit('learnpython')
-#    for submission in subreddit.get_hot(limit=10):
-#        op_text = submission.selftext.lower()
-#        has_praw = any(string in op_text for string in prawWords)
-#        # Test if it contains a PRAW-related question
-#        if submission.id not in already_done and has_praw:
-#            msg = '[PRAW related thread](%s)' % submission.short_link
-#            r.user.send_message('_Daimon_', msg)
-#            already_done.append(submission.id)
-#    time.sleep(1800)
+prawWords = ['testestestestestes', 'test']
+while True:
+    subreddit = r.get_subreddit('test')
+    for submission in subreddit.get_hot(limit=10):
+        op_text = submission.selftext.lower()
+        has_praw = any(string in op_text for string in prawWords)
+        # Test if it contains a PRAW-related question
+        if submission.id not in already_done and has_praw:
+            msg = '[PRAW related thread](%s)' % submission.short_link
+            print('found match')
+            print(submission.permalink)
+            
+            #already_done.append(submission.id)
+    time.sleep(1800)
