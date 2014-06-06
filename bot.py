b929d11f4d6a4f4978753ee575e1f6e7e6cabc42
Brian Collins <bcollins@newrelic.com>
2014-06-05 20:16:17 -0700

    trying to login with test password

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ url = open('url.txt', 'r').readline()
 print url
 
 # todo: reddit API bot functionality
-#  search for "Needs more JPEG!", or variant
+
 #  get the image file
 #  compress to 1 quality and save copy locally
 #  upload to imgur, save url 
@@ -22,8 +22,20 @@ print url
 #  post to reddit
 
 
-# I know you need a user agent so reddit doesn't freak:
-# user_agent = ("needsMoreJPEGBot/0.1 by bcollinsNR")
+r = praw.Reddit('needsMoreJPEGBot/0.1 by bcollinsNR'
+                'Url: https://github.com/bcollinsNR/needsMoreJPEG')
+r.login('needMoarJPEG','0111000001100001011100110111001101110111011011110111001001100100')
+#already_done = []
 
-
-
+#prawWords = ['needs', 'more', 'jpeg']
+#while True:
+#    subreddit = r.get_subreddit('learnpython')
+#    for submission in subreddit.get_hot(limit=10):
+#        op_text = submission.selftext.lower()
+#        has_praw = any(string in op_text for string in prawWords)
+#        # Test if it contains a PRAW-related question
+#        if submission.id not in already_done and has_praw:
+#            msg = '[PRAW related thread](%s)' % submission.short_link
+#            r.user.send_message('_Daimon_', msg)
+#            already_done.append(submission.id)
+#    time.sleep(1800)
